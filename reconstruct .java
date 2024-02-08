f38e63b02a01a303cefa9c2c1f9608bec53207a1
Pratham <89197939+pg348@users.noreply.github.com>
2024-02-08 14:15:21 +0530

    Create reconstruct.java

---
+++ reconstruct .java
@@ -0,0 +1,38 @@
+class Solution {
+    public String originalDigits(String s) {
+
+        int n=s.length();
+        int[] map=new int [26];
+
+        for(char ch:s.toCharArray()){
+            map[ch-'a']++;      
+        }
+
+        int[] digit=new int[10];
+
+        for(int i=0;i<n;i++){
+            digit[0]=map['z'-'a'];
+            digit[6]=map['x'-'a'];
+            digit[4]=map['u'-'a'];
+            digit[2]=map['w'-'a'];
+            digit[8]=map['g'-'a'];
+            digit[5]=map['f'-'a'] - digit[4];
+            digit[7]=map['s'-'a'] - digit[6];
+            digit[3]=map['t'-'a'] - digit[8] - digit[2];
+            digit[1]=map['o'-'a']-digit[2]-digit[4]-digit[0];
+            digit[9]=map['i'-'a']-digit[5]-digit[6]-digit[8];
+        }
+        StringBuilder sb=new StringBuilder("");
+        for(int i=0;i<10;i++){
+            int fre=digit[i];
+            while(fre>0){
+                sb.append(i);
+                fre--;
+            }
+        }
+
+        return sb.toString();
+
+ 
+    }
+}
